959fe9ce4cef7febb031e3e5076d29a0bbc60816
Brigs <abrignoni@gmail.com>
2020-03-05 21:42:31 -0500

    Add files via upload
    SAmsung CMH database support

---
+++ scripts/artifacts/cmh.py
@@ -0,0 +1,40 @@
+import glob
+import json
+import os
+import shutil
+import sqlite3
+
+from scripts.artifact_report import ArtifactHtmlReport
+from scripts.ilapfuncs import logfunc, is_platform_windows
+
+def get_cmh(files_found, report_folder):
+
+    file_found = str(files_found[0])
+    db = sqlite3.connect(file_found)
+    cursor = db.cursor()
+    cursor.execute('''
+    SELECT
+        datetime(datetaken /1000, "unixepoch") as times,
+        latitude,
+        longitude,
+        address_text,
+        uri,
+        _data
+    FROM location_view
+    ''')
+    all_rows = cursor.fetchall()
+    usageentries = len(all_rows)
+    if usageentries > 0:
+        report = ArtifactHtmlReport('Samsung CMH')
+        report.start_artifact_report(report_folder, f'Geodata')
+        report.add_style()
+        data_headers = ('Data Taken', 'Latitude', 'Longitude','Address', 'URI', 'Data Location')
+        data_list = []
+        for row in all_rows:
+            data_list.append((row[0], row[1], row[2], row[3], row[4], row[5]))
+        report.write_artifact_data_table(data_headers, data_list, file_found)
+        report.end_artifact_report()
+    else:
+        logfunc(f'No Samsung_CMH_GeoData_{uid} available')    
+    db.close()
+    return
