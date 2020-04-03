522d3fe7855f1e4fabf585acba148566221bf2cd
Brigs <abrignoni@gmail.com>
2020-04-02 21:12:57 -0400

    Add files via upload
    Updated the URL field data to give it a width limit.

--- scripts/artifacts/chromeOfflinePages.py
+++ scripts/artifacts/chromeOfflinePages.py
@@ -30,7 +30,7 @@ def get_chromeOfflinePages(files_found, report_folder, seeker):
         data_headers = ('Creation Time','Last Access Time', 'Online URL', 'File Path', 'Title', 'Access Count', 'File Size' ) # Don't remove the comma, that is required to make this a tuple as there is only 1 element
         data_list = []
         for row in all_rows:
-            data_list.append((row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
+            data_list.append((row[0],row[1],(textwrap.fill(row[2], width=75)),row[3],row[4],row[5],row[6]))
         report.write_artifact_data_table(data_headers, data_list, file_found)
         report.end_artifact_report()
     else:
