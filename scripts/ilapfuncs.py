56de38cb58bb45ad9fcab07b9c718d6ade485c53
Alexis Bringoni <abrignoni@gmail.com>
2020-07-18 19:52:15 -0400

    Fix window bug on TSV/timeline folder path join

--- scripts/ilapfuncs.py
+++ scripts/ilapfuncs.py
@@ -134,6 +134,7 @@ def html2csv(reportfolderbase):
 
 def tsv(report_folder, data_headers, data_list, tsvname):
     report_folder = report_folder.rstrip('/')
+    report_folder = report_folder.rstrip('\\')
     report_folder_base, tail = os.path.split(report_folder)
     tsv_report_folder = os.path.join(report_folder_base, '_TSV Exports')
     
@@ -150,6 +151,7 @@ def tsv(report_folder, data_headers, data_list, tsvname):
 
 def timeline(report_folder, tlactivity, data_list):
     report_folder = report_folder.rstrip('/')
+    report_folder = report_folder.rstrip('\\')
     report_folder_base, tail = os.path.split(report_folder)
     tl_report_folder = os.path.join(report_folder_base, '_Timeline')
 
