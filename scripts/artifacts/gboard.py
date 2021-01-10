c36d64eeebc074eabe62fde5f49cd7deb139a966
Yogesh Khatri (@swiftforensics) <yogesh@swiftforensics.com>
2021-01-10 00:06:17 -0500

    Update gboard.py

--- scripts/artifacts/gboard.py
+++ scripts/artifacts/gboard.py
@@ -16,7 +16,7 @@ class keyboard_event:
         self.start_date = start_date
         self.end_date = end_date
 
-def get_gboardCache(files_found, report_folder, seeker):
+def get_gboardCache(files_found, report_folder, seeker, wrap_text):
     
     for file_found in files_found:
         file_found = str(file_found)
