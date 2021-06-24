5f828c53629df24f8742e94a848b6f9d0f2359c5
stark4n6 <48143894+stark4n6@users.noreply.github.com>
2021-06-23 23:27:16 -0400

    Add Bluetooth Connections

---
+++ scripts/artifacts/bluetoothConnections.py
@@ -0,0 +1,70 @@
+import csv
+import datetime
+import os
+import re
+import scripts.artifacts.artGlobals 
+
+from scripts.artifact_report import ArtifactHtmlReport
+from scripts.ilapfuncs import logfunc, logdevinfo, tsv, timeline, is_platform_windows
+
+def get_bluetoothConnections(files_found, report_folder, seeker, wrap_text):
+    data_list = []
+    file_found = str(files_found[0])
+    
+    name_value = ''
+    timestamp_value = ''
+    linkkey_value = ''
+    macaddrf = ''
+    first_round = True
+    
+    with open(file_found, "r") as f:
+        for line in f: 
+            
+            p = re.compile(r'(\[[0-9a-f]{2}(?::[0-9a-f]{2}){5}\])', re.IGNORECASE)
+            macaddr = re.findall(p, line)
+            if macaddr:
+                try:
+                    if first_round == True:
+                        first_round = False
+                    else:
+                        data_list.append((timestamp_value, name_value, macaddrf, linkkey_value))
+                        name_value = ''
+                        timestamp_value = ''
+                        linkkey_value = ''
+                    macaddrf = macaddr[0].strip('[]')
+                except:
+                    pass
+                
+            splits = line.split(' = ')
+                
+            if splits[0] == 'Name':
+                key = 'Name'
+                name_value = splits[1].strip()
+                
+            if splits[0] == 'Timestamp':
+                key = 'Timestamp'
+                timestamp_value = splits[1].strip()
+                timestamp_value = datetime.datetime.fromtimestamp(int(timestamp_value)).strftime('%Y-%m-%d %H:%M:%S')
+                
+            if splits[0] == 'LinkKey':
+                key = 'LinkKey'
+                linkkey_value = splits[1].strip()
+    
+    data_list.append((timestamp_value, name_value, macaddrf, linkkey_value))
+        
+    if len(data_list) > 0:
+        report = ArtifactHtmlReport('Bluetooth Connections')
+        report.start_artifact_report(report_folder, f'Bluetooth Connections')
+        report.add_script()
+        data_headers = ('First Connected Timestamp','Device Name','MAC Address','Link Key')
+        report.write_artifact_data_table(data_headers, data_list, file_found)
+        report.end_artifact_report()
+        
+        tsvname = f'BBluetooth Connections'
+        tsv(report_folder, data_headers, data_list, tsvname)
+        
+        tlactivity = f'Bluetooth Connections'
+        timeline(report_folder, tlactivity, data_list, data_headers)
+    else:
+        logfunc(f'No Bluetooth Connections data available')
+   
