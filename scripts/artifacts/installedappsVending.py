4f7e2375d10bca2c1d26f362092e29b21f80fe8e
Pagano <48143894+stark4n6@users.noreply.github.com>
2021-09-17 15:36:58 -0400

    Update installedappsVending.py

--- scripts/artifacts/installedappsVending.py
+++ scripts/artifacts/installedappsVending.py
@@ -8,45 +8,99 @@ def get_installedappsVending(files_found, report_folder, seeker, wrap_text):
     file_found = str(files_found[0])
     db = open_sqlite_db_readonly(file_found)
     cursor = db.cursor()
-    cursor.execute('''
-    SELECT
-        CASE
-            first_download_ms
-            WHEN
-                "0" 
-            THEN
-                "0" 
-            ELSE
-                datetime(first_download_ms / 1000, "unixepoch")
-        END AS "fdl",
-        package_name,
-        title,
-        install_reason,
-        auto_update
-    FROM appstate  
-    ''')
+    
+    columns = [i[1] for i in cursor.execute('PRAGMA table_info(appstate)')]
+    
+    if 'install_reason' in columns:
+        cursor.execute('''
+        SELECT
+            CASE first_download_ms
+                WHEN '0' THEN '' 
+                ELSE
+                    datetime(first_download_ms / 1000, "unixepoch")
+            END AS 'First_Download_DT',
+            package_name,
+            title,
+            install_reason,
+            CASE last_update_timestamp_ms
+                WHEN '0' THEN '' 
+                ELSE
+                    datetime(last_update_timestamp_ms / 1000, "unixepoch")
+            END AS 'Last_Updated_DT',
+            CASE auto_update
+                WHEN '0' THEN ''
+                WHEN '1' THEN 'Yes'
+            END AS 'Auto_Updated',
+            account
+        FROM appstate  
+        ''')
 
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        report = ArtifactHtmlReport('Installed Apps (Vending)')
-        report.start_artifact_report(report_folder, 'Installed Apps (Vending)')
-        report.add_script()
-        data_headers = ('First Download','Package Name', 'Title','Install Reason', 'Auto Update?')
-        data_list = []
-        for row in all_rows:
-            data_list.append((row[0], row[1], row[2], row[3], row[4]))
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        if usageentries > 0:
+            report = ArtifactHtmlReport('Installed Apps (Vending)')
+            report.start_artifact_report(report_folder, 'Installed Apps (Vending)')
+            report.add_script()
+            data_headers = ('First Download','Package Name', 'Title','Install Reason', 'Last Updated', 'Auto Update?', 'Account')
+            data_list = []
+            for row in all_rows:
+                data_list.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
 
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
-        
-        tsvname = f'installed apps vending'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = f'Installed Apps Vending'
-        timeline(report_folder, tlactivity, data_list, data_headers)        
+            report.write_artifact_data_table(data_headers, data_list, file_found)
+            report.end_artifact_report()
+            
+            tsvname = f'installed apps vending'
+            tsv(report_folder, data_headers, data_list, tsvname)
+            
+            tlactivity = f'Installed Apps Vending'
+            timeline(report_folder, tlactivity, data_list, data_headers)        
+        else:
+                logfunc('No Installed Apps data available')
+    
     else:
-            logfunc('No Installed Apps data available')
+        cursor.execute('''
+        SELECT
+            CASE first_download_ms
+                WHEN '0' THEN '' 
+                ELSE
+                    datetime(first_download_ms / 1000, "unixepoch")
+            END AS 'First_Download_DT',
+            package_name,
+            title,
+            CASE last_update_timestamp_ms
+                WHEN '0' THEN '' 
+                ELSE
+                    datetime(last_update_timestamp_ms / 1000, "unixepoch")
+            END AS 'Last_Updated_DT',
+            CASE auto_update
+                WHEN '0' THEN ''
+                WHEN '1' THEN 'Yes'
+            END AS 'Auto_Updated',
+            account
+        FROM appstate  
+        ''')
+
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        if usageentries > 0:
+            report = ArtifactHtmlReport('Installed Apps (Vending)')
+            report.start_artifact_report(report_folder, 'Installed Apps (Vending)')
+            report.add_script()
+            data_headers = ('First Download','Package Name', 'Title', 'Last Updated', 'Auto Update?', 'Account')
+            data_list = []
+            for row in all_rows:
+                data_list.append((row[0], row[1], row[2], row[3], row[4], row[5]))
+
+            report.write_artifact_data_table(data_headers, data_list, file_found)
+            report.end_artifact_report()
+            
+            tsvname = f'installed apps vending'
+            tsv(report_folder, data_headers, data_list, tsvname)
+            
+            tlactivity = f'Installed Apps Vending'
+            timeline(report_folder, tlactivity, data_list, data_headers)        
+        else:
+                logfunc('No Installed Apps data available')
     
     db.close()
     return
