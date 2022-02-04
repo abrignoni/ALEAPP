d5732f37f1137ffaee65cca76a8be7008044d95e
abrignoni <abrignoni@gmail.com>
2022-02-04 16:19:51 -0500

    Update torThumbs.py
    Added relative pathing for recovered media.

--- scripts/artifacts/torThumbs.py
+++ scripts/artifacts/torThumbs.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from PIL import Image
 
 from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import timeline, tsv, is_platform_windows, open_sqlite_db_readonly, is_platform_windows
+from scripts.ilapfuncs import timeline, tsv, is_platform_windows, open_sqlite_db_readonly, is_platform_windows, media_to_html
 
 
 def get_torThumbs(files_found, report_folder, seeker, wrap_text):
@@ -24,7 +24,9 @@ def get_torThumbs(files_found, report_folder, seeker, wrap_text):
         img = Image.open(file_found) 
         img.save(savepath,'png')
         
-        thumb = f'<img src="{savepath}"width="300"></img>'
+        medialist = (savepath,)
+        thumb = media_to_html(savepath, medialist, report_folder)
+        #thumb = f'<img src="{savepath}"width="300"></img>'
         
         platform = is_platform_windows()
         if platform:
