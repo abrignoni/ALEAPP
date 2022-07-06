f0d21dedd5a9a2bfc1d5151c92ef8ddb9faaa8ed
abrignoni <abrignoni@gmail.com>
2022-07-06 14:59:40 -0400

    Usages-history.xml
    Support for legacy app usage xml data store.

--- scripts/artifacts/contacts.py
+++ scripts/artifacts/contacts.py
@@ -66,7 +66,7 @@ def get_contacts(files_found, report_folder, seeker, wrap_text):
             tsvname = f'Contacts'
             tsv(report_folder, data_headers, data_list, tsvname, source_file)
 
-            tlactivity = f'Contaccts'
+            tlactivity = f'Contacts'
             timeline(report_folder, tlactivity, data_list, data_headers)
             
         else:
