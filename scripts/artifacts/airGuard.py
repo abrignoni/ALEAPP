1d534719846ba3177f3b1341a7406d433cef4387
stark4n6 <48143894+stark4n6@users.noreply.github.com>
2023-09-19 16:08:23 -0400

    Update airGuard.py
    - Compatibility for 2 versions of tables
    - Add parser for Scans

--- scripts/artifacts/airGuard.py
+++ scripts/artifacts/airGuard.py
@@ -2,7 +2,7 @@ import sqlite3
 import textwrap
 
 from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly, kmlgen
+from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly, kmlgen, does_table_exist
 
 def get_airGuard(files_found, report_folder, seeker, wrap_text):
     
@@ -11,55 +11,115 @@ def get_airGuard(files_found, report_folder, seeker, wrap_text):
         if not file_found.endswith('attd_db'):
             continue # Skip all other files
     
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    cursor.execute('''
-    SELECT
-    device.lastSeen AS "Last Time Device Seen",
-    beacon.receivedAt AS "Time (Local)",
-    beacon.deviceAddress AS "Device MAC Address",
-    beacon.longitude AS "Latitude",
-    beacon.latitude as "Longitude",
-    beacon.rssi AS "Signal Strength (RSSI)",
-    device.firstDiscovery AS "First Time Device Seen",
-    device.lastNotificationSent as "Last Time User Notified"
-    FROM
-    beacon
-    LEFT JOIN device on device.address=beacon.deviceAddress
-    ''')
-    
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        report = ArtifactHtmlReport('AirGuard AirTag Tracker')
-        report.start_artifact_report(report_folder, 'AirGuard AirTag Tracker')
-        report.add_script()
-        data_headers = ('Last Time Device Seen','Time (Local)','Device MAC Address','Latitude','Longitude','Signal Strength (RSSI)','First Time Device Seen','Last Time User Notified') 
-        data_headers_kml = ('Timestamp','Time (Local)','Device MAC Address','Latitude','Longitude','Signal Strength (RSSI)','First Time Device Seen','Last Time User Notified') 
-        data_list = []
-        for row in all_rows:
-            last_time_dev_seen = str(row[0]).replace("T", " ")
-            time_local = str(row[1]).replace("T", " ")
-            first_time_dev_seen = str(row[6]).replace("T", " ")
-            last_time_user_notified = str(row[7]).replace("T", " ")
-            data_list.append((last_time_dev_seen,time_local,row[2],row[3],row[4],row[5],first_time_dev_seen,last_time_user_notified))
+        db = open_sqlite_db_readonly(file_found)
+        location_table_exists = does_table_exist(db, 'location')
+        cursor = db.cursor()
+        if location_table_exists:
+            cursor.execute('''
+            SELECT
+            device.lastSeen AS "Last Time Device Seen",
+            beacon.receivedAt AS "Time (Local)",
+            beacon.deviceAddress AS "Device MAC Address",
+            location.longitude AS "Latitude",
+            location.latitude as "Longitude",
+            beacon.rssi AS "Signal Strength (RSSI)",
+            device.deviceType AS "Device Type",
+            device.firstDiscovery AS "First Time Device Seen",
+            device.lastNotificationSent as "Last Time User Notified"
+            FROM
+            beacon
+            LEFT JOIN device on device.address = beacon.deviceAddress
+            LEFT JOIN location on location.locationId = beacon.locationId
+            ''')
+        else:    
+            cursor.execute('''
+            SELECT
+            device.lastSeen AS "Last Time Device Seen",
+            beacon.receivedAt AS "Time (Local)",
+            beacon.deviceAddress AS "Device MAC Address",
+            beacon.longitude AS "Latitude",
+            beacon.latitude as "Longitude",
+            beacon.rssi AS "Signal Strength (RSSI)",
+            device.deviceType AS "Device Type",
+            device.firstDiscovery AS "First Time Device Seen",
+            device.lastNotificationSent as "Last Time User Notified"
+            FROM
+            beacon
+            LEFT JOIN device on device.address=beacon.deviceAddress
+            ''')
+        
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        if usageentries > 0:
+            report = ArtifactHtmlReport('AirGuard AirTag Tracker')
+            report.start_artifact_report(report_folder, 'AirGuard AirTag Tracker')
+            report.add_script()
+            data_headers = ('Last Time Device Seen','Time (Local)','Device MAC Address','Latitude','Longitude','Signal Strength (RSSI)','Device Type','First Time Device Seen','Last Time User Notified') 
+            data_headers_kml = ('Timestamp','Time (Local)','Device MAC Address','Latitude','Longitude','Signal Strength (RSSI)','Device Type','First Time Device Seen','Last Time User Notified') 
+            data_list = []
+            for row in all_rows:
+                last_time_dev_seen = str(row[0]).replace("T", " ")
+                time_local = str(row[1]).replace("T", " ")
+                first_time_dev_seen = str(row[7]).replace("T", " ")
+                last_time_user_notified = str(row[8]).replace("T", " ")
+                data_list.append((last_time_dev_seen,time_local,row[2],row[3],row[4],row[5],row[6],first_time_dev_seen,last_time_user_notified))
+                
+            report.write_artifact_data_table(data_headers, data_list, file_found)
+            report.end_artifact_report()
             
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
+            tsvname = f'AirGuard AirTag Tracker'
+            tsv(report_folder, data_headers, data_list, tsvname)
+            
+            tlactivity = f'AirGuard AirTag Tracker'
+            timeline(report_folder, tlactivity, data_list, data_headers)
+            
+            kmlactivity = 'AirGuard AirTag Tracker'
+            kmlgen(report_folder, kmlactivity, data_list, data_headers_kml)
+            
+        else:
+            logfunc('No AirGuard AirTag Tracker data available')
+            
+        cursor.execute('''   
+        SELECT
+        startDate,
+        endDate,
+        duration AS "Duration (Seconds)",
+        noDevicesFound,
+        CASE isManual
+            WHEN 0 THEN 'No'
+            WHEN 1 THEN 'Yes'
+        END,
+        scanMode
+        FROM scan
+        ''')
+
+        all_rows = cursor.fetchall()
+        usageentries = len(all_rows)
+        if usageentries > 0:
+            report = ArtifactHtmlReport('AirGuard AirTag Scans')
+            report.start_artifact_report(report_folder, 'AirGuard AirTag Scans')
+            report.add_script()
+            data_headers = ('Start Scan Timestamp','End Scan Timestamp','Duration (Seconds)','Devices Found','Manual Scan?','Scan Mode') 
+            data_headers_kml = ('Timestamp','End Scan Timestamp','Duration (Seconds)','Devices Found','Manual Scan?','Scan Mode') 
+            data_list = []
+            for row in all_rows:
+                start_scan_ts = str(row[0]).replace("T", " ")
+                end_scan_ts = str(row[1]).replace("T", " ")
+                data_list.append((start_scan_ts,end_scan_ts,row[2],row[3],row[4],row[5]))
+                
+            report.write_artifact_data_table(data_headers, data_list, file_found)
+            report.end_artifact_report()
+            
+            tsvname = f'AirGuard AirTag Scans'
+            tsv(report_folder, data_headers, data_list, tsvname)
+            
+            tlactivity = f'AirGuard AirTag Scans'
+            timeline(report_folder, tlactivity, data_list, data_headers)
+            
+        else:
+            logfunc('No AirGuard AirTag Scans data available')
         
-        tsvname = f'AirGuard AirTag Tracker'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = f'AirGuard AirTag Tracker'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-        
-        kmlactivity = 'AirGuard AirTag Tracker'
-        kmlgen(report_folder, kmlactivity, data_list, data_headers_kml)
-        
-    else:
-        logfunc('No AirGuard AirTag Tracker data available')
-        
-    db.close()
+        db.close()
 
 __artifacts__ = {
         "AirGuard": (
