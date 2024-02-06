5b8e89ee74e7a703fcedc6044e11c8cd7f7ef0b5
patrickdalla <patrick.dalla@gmail.com>
2024-02-06 09:54:22 -0400

    from chrome import get_browser_name

--- scripts/artifacts/chromeCookies.py
+++ scripts/artifacts/chromeCookies.py
@@ -4,19 +4,7 @@ import textwrap
 
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, get_next_unused_name, open_sqlite_db_readonly
-
-def get_browser_name(file_name):
-
-    if 'brave' in file_name.lower():
-        return 'Brave'
-    elif 'microsoft' in file_name.lower():
-        return 'Edge'
-    elif 'opera' in file_name.lower():
-        return 'Opera'
-    elif 'android.chrome' in file_name.lower():
-        return 'Chrome'
-    else:
-        return 'Unknown'
+from chrome import get_browser_name
 
 def get_chromeCookies(files_found, report_folder, seeker, wrap_text):
     
