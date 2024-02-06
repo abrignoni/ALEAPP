5715274676804477a0a3ea9e3c2fd33fe5db46d9
patrickdalla <patrick.dalla@gmail.com>
2024-02-06 09:54:09 -0400

    from chrome import get_browser_name

--- scripts/artifacts/chromeBookmarks.py
+++ scripts/artifacts/chromeBookmarks.py
@@ -5,18 +5,7 @@ import os
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, get_next_unused_name
 
-def get_browser_name(file_name):
-
-    if 'brave' in file_name.lower():
-        return 'Brave'
-    elif 'microsoft' in file_name.lower():
-        return 'Edge'
-    elif 'opera' in file_name.lower():
-        return 'Opera'
-    elif 'android.chrome' in file_name.lower():
-        return 'Chrome'
-    else:
-        return 'Unknown'
+from chrome import get_browser_name
 
 def get_chromeBookmarks(files_found, report_folder, seeker, wrap_text, time_offset):
     
