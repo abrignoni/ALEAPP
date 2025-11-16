a90d7701cafd970164ae5b30f383d112aa19a9ed
kalink0 <kalinko@be-binary.de>
2025-11-16 17:31:19 +0100

    Added support for Messages in App FairEmail and refactroed the other fairemail parsers for use with lava

--- scripts/artifacts/FairEmail.py
+++ scripts/artifacts/FairEmail.py
@@ -3,9 +3,9 @@
 # Version: 0.0.1
 # 
 # Tested with the following versions:
-# 2024-04-20: Android 14, App: x.x.x
+# 2024-04-20: Android 14, App: 1.2178
 
-# Requirements:  datetime
+# Requirements:  
 
 
 
@@ -13,48 +13,59 @@
 __artifacts_v2__ = {
 
     
-    "FairEmailAccounts": {
+    "get_fair_mail_accounts": {
         "name": "FairEmail - Accounts",
         "description": "FairEmail Accounts",
         "author": "Marco Neumann {kalinko@be-binary.de}",
-        "version": "0.0.1",
-        "date": "2025-03-08",
+        "version": "0.0.2",
+        "creation_date": "2025-03-08",
+        "last_update_date": "2025-11-15",
         "requirements": "none",
         "category": "FairCode FairEmail App",
         "notes": "",
         "paths": ('*/eu.faircode.email/databases/fairemail*'),
-        "function": "get_fairemail_accounts"
+        "output_types": ["standard"],
+        "artifact_icon": "inbox"
     },
-        "FairEmailContacts": {
+    "get_fair_mail_contacts": {
         "name": "FairEmail - Contacts",
         "description": "FairEmail Contacts",
         "author": "Marco Neumann {kalinko@be-binary.de}",
-        "version": "0.0.1",
-        "date": "2025-03-08",
+        "version": "0.0.2",
+        "creation_date": "2025-03-08",
+        "last_update_date": "2025-11-15",
         "requirements": "none",
         "category": "FairCode FairEmail App",
         "notes": "",
         "paths": ('*/eu.faircode.email/databases/fairemail*'),
-        "function": "get_fairemail_contacts"
+        "output_types": ["standard"],
+        "artifact_icon": "users"
+    }
+    ,
+    "get_fair_mail_messages": {
+        "name": "FairEmail - Messages",
+        "description": "FairEmail Messages",
+        "author": "Marco Neumann {kalinko@be-binary.de}",
+        "version": "0.0.1",
+        "creation_date": "2025-11-16",
+        "last_update_date": "2025-11-16",
+        "requirements": "none",
+        "category": "FairCode FairEmail App",
+        "notes": "",
+        "paths": ('*/eu.faircode.email/databases/fairemail*', '*/eu.faircode.email/files/attachments/*', '*/eu.faircode.email/files/messages/*'),
+        "output_types": ["standard"],
+        "artifact_icon": "mail"
     }
 }
 
+from scripts.ilapfuncs import artifact_processor, convert_unix_ts_to_utc, get_sqlite_db_records
 
 
-import datetime
-
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly
-
-
-def get_fairemail_accounts(files_found, report_folder, seeker, wrap_text, time_offset):
-    logfunc("Processing data for FairEmail App - Accounts")
+@artifact_processor
+def get_fair_mail_accounts(files_found, _report_folder, _seeker, _wrap_text):
     files_found = [x for x in files_found if not x.endswith('wal') and not x.endswith('shm')]
-    
-    file_found = str(files_found[0])
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    cursor.execute('''
+        
+    query = ('''
         SELECT
         identity.account, identity.name, identity.email, identity.display, identity.signature, account.host, account.port, account.user, account.password, account.name, account.created, account.last_connected 
         FROM account
@@ -62,56 +73,35 @@ def get_fairemail_accounts(files_found, report_folder, seeker, wrap_text, time_o
         ON account.id = identity.account
     ''')
 
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        logfunc(f"Found {usageentries}  FairEmail - Accounts")
-        description = f"Existing accounts in FairEmail App."
-        report = ArtifactHtmlReport('FairEmail - Accounts')
-        report.start_artifact_report(report_folder, 'FairEmail - Accounts', description)
-        report.add_script()
-        data_headers = ('Account ID', 'Name', 'E-Mail Address', 'Display Name', 'Signature', 'IMAP Server', 'IMAP Port', 'Username', 'Password', 'Account Name', 'Created Date', 'Last Connected Date')
-        data_list = []
-        for row in all_rows:
-            id = row[0]
-            name = row[1]
-            email = row[2]
-            display_name = row[3]
-            signature = row[4]
-            server = row[5]
-            port = row[6]
-            username = row[7]
-            password = row[8]
-            account_name = row[9]
-            creationdate = datetime.datetime.fromtimestamp(row[10]/1000).strftime('%Y-%m-%d %H:%M:%S')
-            lastconnecteddate = datetime.datetime.fromtimestamp(row[11]/1000).strftime('%Y-%m-%d %H:%M:%S')
+    db_records = get_sqlite_db_records(str(files_found[0]), query)
 
-            data_list.append((id, name, email, display_name, signature, server, port, username, password, account_name, creationdate, lastconnecteddate))
+    data_list = []
 
-        tableID = 'FairEmail_accounts'
+    for row in db_records:
+        account_id = row[0]
+        name = row[1]
+        email = row[2]
+        display_name = row[3]
+        signature = row[4]
+        server = row[5]
+        port = row[6]
+        username = row[7]
+        password = row[8]
+        account_name = row[9]
+        creationdate = convert_unix_ts_to_utc(row[10]/1000)
+        lastconnecteddate = convert_unix_ts_to_utc(row[11]/1000)
 
-        report.write_artifact_data_table(data_headers, data_list, ','.join(files_found))
-        report.end_artifact_report()
+        data_list.append((account_id, name, email, display_name, signature, server, port, username, password, account_name, creationdate, lastconnecteddate))
 
-        tsvname = f'FairEmail - Accounts'
-        tsv(report_folder, data_headers, data_list, tsvname)
+    data_headers = ('Account ID', 'Name', 'E-Mail Address', 'Display Name', 'Signature', 'IMAP Server', 'IMAP Port', 'Username', 'Password', 'Account Name', 'Created Date', 'Last Connected Date')
 
-        tlactivity = f'FairEmail - Accounts'
-        timeline(report_folder, tlactivity, data_list, data_headers)
+    return data_headers, data_list, files_found[0]
 
-    else:
-        logfunc('No FairEmail Account data found!')
-
-    db.close()
-
-def get_fairemail_contacts(files_found, report_folder, seeker, wrap_text, time_offset):
-    logfunc("Processing data for FairEmail App - Contacts")
+@artifact_processor
+def get_fair_mail_contacts(files_found, _report_folder, _seeker, _wrap_text):
     files_found = [x for x in files_found if not x.endswith('wal') and not x.endswith('shm')]
     
-    file_found = str(files_found[0])
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    cursor.execute('''
+    query = ('''
         SELECT
         contact.id, contact.name, contact.email, contact.times_contacted, contact.first_contacted, contact.last_contacted, account.name, account.user
         FROM account
@@ -119,40 +109,99 @@ def get_fairemail_contacts(files_found, report_folder, seeker, wrap_text, time_o
         ON account.id = contact.account
     ''')
 
-    all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        logfunc(f"Found {usageentries}  FairEmail - Contacts")
-        description = f"Contacted contacts in FairEmail App."
-        report = ArtifactHtmlReport('FairEmail - Contacts')
-        report.start_artifact_report(report_folder, 'FairEmail - Contacts', description)
-        report.add_script()
-        data_headers = ('Contact ID', 'Contact Display Name', 'E-Mail Address', 'Times Contacted', 'First Contacted', 'Last Contacted', 'Used Account Name', 'Used Account Username')
-        data_list = []
-        for row in all_rows:
-            id = row[0]
-            name = row[1]
-            email = row[2]
-            times_contacted = row[3]
-            firstcontacteddate = datetime.datetime.fromtimestamp(row[4]/1000).strftime('%Y-%m-%d %H:%M:%S')
-            lastcontacteddate = datetime.datetime.fromtimestamp(row[5]/1000).strftime('%Y-%m-%d %H:%M:%S')
-            account_name = row[6]
-            username = row[7]
+    db_records = get_sqlite_db_records(str(files_found[0]), query)
+    
 
-            data_list.append((id, name, email, times_contacted, firstcontacteddate, lastcontacteddate, account_name, username))
+    data_list = []
+    for row in db_records:
+        contact_id = row[0]
+        name = row[1]
+        email = row[2]
+        times_contacted = row[3]
+        firstcontacteddate = convert_unix_ts_to_utc(row[4]/1000)
+        lastcontacteddate = convert_unix_ts_to_utc(row[5]/1000)
+        account_name = row[6]
+        username = row[7]
 
-        tableID = 'FairEmail_Contacts'
+        data_list.append((contact_id, name, email, times_contacted, firstcontacteddate, lastcontacteddate, account_name, username))
+        
+    data_headers = ('Contact ID', 'Contact Display Name', 'E-Mail Address', 'Times Contacted', 'First Contacted', 'Last Contacted', 'Used Account Name', 'Used Account Username')
 
-        report.write_artifact_data_table(data_headers, data_list, ','.join(files_found))
-        report.end_artifact_report()
+    return data_headers, data_list, files_found[0]
 
-        tsvname = f'FairEmail - Contacts'
-        tsv(report_folder, data_headers, data_list, tsvname)
+@artifact_processor
+def get_fair_mail_messages(files_found, _report_folder, _seeker, _wrap_text):
+    
+    # Get the different files found and store them in corressponindg lists to work with them
+    main_db = ''
 
-        tlactivity = f'FairEmail - Contacts'
-        timeline(report_folder, tlactivity, data_list, data_headers)
+    for file_found in files_found:
+        file_found = str(file_found)
+    
+        if file_found.endswith('fairemail'):
+            main_db = file_found
 
-    else:
-        logfunc('No FairEmail Contact data found!')
 
-    db.close()
+    query = ('''
+        SELECT m.id [Message ID],
+        account.user [Account],
+        folder.name [Folder], 
+        json_extract(jefrom.value, '$.address') [From Address],
+        json_extract(jefrom.value, '$.personal') [From Name],
+        json_extract(jeto.value, '$.address') [To Address],
+        json_extract(jeto.value, '$.personal') [To Name],
+        json_extract(jecc.value, '$.address') [CC Address],
+        json_extract(jecc.value, '$.personal') [CC Name],
+        json_extract(jebcc.value, '$.address') [BCC Address],
+        json_extract(jebcc.value, '$.personal') [BCC Name],
+        json_extract(return_path, '$[0].address') [Return Path],
+        subject [Subject],
+        sent [Timestamp Sent],
+        received [Timestamp Received],
+        stored [Timestamp Stored], 
+        seen [Read?],
+        attachments [# of Attachements], 
+        infrastructure [Backend Infrastructure]
+        FROM message m
+        LEFT JOIN json_each("from") jefrom
+        LEFT JOIN json_each("to") jeto
+        LEFT JOIN json_each("cc") jecc
+        LEFT JOIN json_each("bcc") jebcc
+        INNER JOIN account
+        ON account.id = m.account
+		INNER JOIN folder
+		ON folder.id = m.folder
+    ''')
+
+    db_records = get_sqlite_db_records(main_db, query)
+
+
+    data_list = []
+
+
+    for row in db_records:
+        message_id = row[0]
+        account = row[1]
+        folder = row[2]
+        address_from = row[3]
+        name_from = row[4]
+        address_to = row[5]
+        name_to = row[6]
+        address_cc = row[7]
+        name_cc = row[8]
+        address_bcc = row[9]
+        name_bcc = row[10]
+        return_path = row[11]
+        subject = row[12]
+        sent = convert_unix_ts_to_utc(row[13] or 0/1000) if row[13] is not None else None
+        received = convert_unix_ts_to_utc(row[14]/1000)
+        stored = convert_unix_ts_to_utc(row[15]/1000)
+        seen = row[16]
+        attachments = row[17]
+        infrastructure = row[18]
+
+        data_list.append((message_id, account, folder, address_from, name_from, address_to, name_to, address_cc, name_cc, address_bcc, name_bcc, return_path, subject, sent, received, stored, seen, attachments, infrastructure))
+
+    data_headers = ('Message ID', 'Mail Account User', 'Mail Folder Name', 'Sender Address', 'Sender Name', 'Receiver Address', 'Receiver Name', 'CC Address', 'CC Name', 'BCC Address', 'BCC Name', 'Return Path', 'Subject', 'Sent', 'Received', 'Stored', 'Seen', 'Attachments', 'Infrastructure')
+
+    return data_headers, data_list, main_db
