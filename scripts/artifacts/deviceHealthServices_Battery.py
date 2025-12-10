e6ec15702154143b008740dd2b75949da18ec6c6
SchoolBoyJoJo <jonathanprb060504@gmail.com>
2025-12-10 14:46:42 +0700

    fix Error Turbo_Bluetooth & Turbo_Battery

--- scripts/artifacts/deviceHealthServices_Battery.py
+++ scripts/artifacts/deviceHealthServices_Battery.py
@@ -31,7 +31,6 @@ __artifacts_v2__ = {
 
 import sqlite3
 import textwrap
-import os
 
 from packaging import version
 from scripts.artifact_report import ArtifactHtmlReport
@@ -47,8 +46,8 @@ def Turbo_Battery(files_found, report_folder, seeker, wrap_text):
         file_found = str(file_found)
         if file_found.lower().endswith('turbo.db'):
             turbo_db = str(file_found)
-            source_file_turbo = os.path.basename(file_found)
-        
+            source_file_turbo = file_found.replace(seeker.directory, '')
+           
             db = open_sqlite_db_readonly(turbo_db)
             cursor = db.cursor()
             cursor.execute('''
@@ -80,51 +79,58 @@ def Turbo_Battery(files_found, report_folder, seeker, wrap_text):
                     if timestamp is None:
                         pass
                     else:
-                        timestamp = convert_utc_human_to_timezone(convert_ts_human_to_utc(timestamp),time_offset)
+                        time_offset = row[4] if row[4] not in (None, '') else 'UTC'
+                        timestamp = convert_utc_human_to_timezone(convert_ts_human_to_utc(timestamp), time_offset)
+
                     data_list.append((timestamp,row[1],row[2],row[3],row[4],file_found))
+
             
             db.close()
-            
+               
     data_headers = (('Timestamp', 'datetime'),'Battery Level','Charge Type','Battery Saver','Timezone','Source')
         
     return data_headers, data_list, source_file_turbo
-            
+               
 @artifact_processor
 def Turbo_Bluetooth(files_found, report_folder, seeker, wrap_text):     
     source_file_bluetooth = ''
-    turbo_db = ''
     data_list = []
 
-    if file_found.lower().endswith('bluetooth.db'):
-        bluetooth_db = str(file_found)
-        source_file_bluetooth = file_found.replace(seeker.directory, '')
-    
-        db = open_sqlite_db_readonly(bluetooth_db)
-        cursor = db.cursor()
-        cursor.execute('''
-        select
-        datetime(timestamp_millis/1000,'unixepoch'),
-        bd_addr,
-        device_identifier,
-        battery_level,
-        volume_level,
-        time_zone
-        from battery_event
-        join device_address on battery_event.device_idx = device_address.device_idx
-        ''')
+    for file_found in files_found:
+        file_found = str(file_found)
+        if file_found.lower().endswith('bluetooth.db'):
+            bluetooth_db = file_found
+            source_file_bluetooth = file_found.replace(seeker.directory, '')
 
-        all_rows = cursor.fetchall()
-        usageentries = len(all_rows)
-        if usageentries > 0:
-            for row in all_rows:
-                timestamp = row[0]
-                if timestamp is None:
-                    pass
-                else:
-                    timestamp = convert_utc_human_to_timezone(convert_ts_human_to_utc(timestamp),time_offset)
-                data_list.append((timestamp,row[1],row[2],row[3],row[4],row[5],file_found))
-        db.close()
-        
+            db = open_sqlite_db_readonly(bluetooth_db)
+            cursor = db.cursor()
+            cursor.execute('''
+            select
+            datetime(timestamp_millis/1000,'unixepoch'),
+            bd_addr,
+            device_identifier,
+            battery_level,
+            volume_level,
+            time_zone
+            from battery_event
+            join device_address on battery_event.device_idx = device_address.device_idx
+            ''')
+
+            all_rows = cursor.fetchall()
+            if len(all_rows) > 0:
+                for row in all_rows:
+                    timestamp = row[0]
+                    if timestamp is not None:
+                        try:
+                            timestamp = convert_utc_human_to_timezone(convert_ts_human_to_utc(timestamp), time_offset)
+                        except:
+                            pass
+
+                    data_list.append((timestamp, row[1], row[2], row[3], row[4], row[5], file_found))
+
+            db.close()
+            break  # stop after matching db found
+
     data_headers = (('Timestamp','datetime'),'BT Device MAC Address','BT Device ID','Battery Level','Volume Level','Timezone','Source')
 
     return data_headers, data_list, source_file_bluetooth
