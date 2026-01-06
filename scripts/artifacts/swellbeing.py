55e8f8156179bda6a4153691e585535204eeca29
Kevin - Stark 4N6 <48143894+stark4n6@users.noreply.github.com>
2026-01-05 21:58:07 -0500

    Update swellbeing.py

--- scripts/artifacts/swellbeing.py
+++ scripts/artifacts/swellbeing.py
@@ -1,5 +1,5 @@
 from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly
+from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly
 
 def get_swellbeing(files_found, report_folder, seeker, wrap_text):
 
