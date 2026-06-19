7f8ae4fe04462d9d3917dad5e7c570b4d1b80fb1
Brigs <abrignoni@gmail.com>
2026-06-19 18:22:43 -0400

    Convert scontextLog, usageHistory, burnerSubscription, burnerContacts to LAVA @artifact_processor

--- scripts/artifacts/scontextLog.py
+++ scripts/artifacts/scontextLog.py
@@ -1,4 +1,4 @@
-# pylint: disable=W0611,W0613,W1309
+# pylint: disable=W0613
 __artifacts_v2__ = {
     "get_scontextLog": {
         "name": "scontextLog",
@@ -10,59 +10,34 @@ __artifacts_v2__ = {
         "category": "App Interaction",
         "notes": "",
         "paths": ('*/com.samsung.android.providers.context/databases/ContextLog.db',),
-        "output_types": None,
+        "output_types": "standard",
         "artifact_icon": "package",
-        "function": "get_scontextLog",
     }
 }
 
-import sqlite3
-import textwrap
+import datetime
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly
+from scripts.ilapfuncs import artifact_processor, open_sqlite_db_readonly
 
+
+@artifact_processor
 def get_scontextLog(files_found, report_folder, seeker, wrap_text):
-    
-    file_found = str(files_found[0])
-    db = open_sqlite_db_readonly(file_found)
+
+    source_path = str(files_found[0])
+    db = open_sqlite_db_readonly(source_path)
     cursor = db.cursor()
     cursor.execute('''
-    SELECT
-    CASE WHEN starttime>0 THEN datetime(starttime /1000, 'UNIXEPOCH')
-         ELSE ""
-    END as date2,
-    CASE WHEN stoptime>0 THEN datetime(stoptime /1000, 'UNIXEPOCH')
-         ELSE ""
-    END as date3,
-    time_zone,
-    app_id,
-    app_sub_id,
-    duration,
-    duration/1000 as duraton_in_secs
-    from use_app
+        SELECT starttime, stoptime, time_zone, app_id, app_sub_id, duration, duration/1000
+        FROM use_app
     ''')
-
     all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        report = ArtifactHtmlReport('Samsung Context Log')
-        report.start_artifact_report(report_folder, 'Samsung Context Log')
-        report.add_script()
-        data_headers = ('Start Time', 'Stop Time','Timezone', 'App ID', 'APP Sub ID', 'Duration', 'Duration in Secs')
-        data_list = []
-        for row in all_rows:
-            data_list.append((row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
-
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
-        
-        tsvname = f'samsung contextlog'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = f'Samsung Context Log'
-        timeline(report_folder, tlactivity, data_list, data_headers) 
-    else:
-        logfunc('No Samsung Context Log data available')
-    
     db.close()
+
+    data_list = []
+    for row in all_rows:
+        start = datetime.datetime.fromtimestamp(int(row[0]) / 1000, datetime.timezone.utc) if row[0] and int(row[0]) > 0 else ''
+        stop = datetime.datetime.fromtimestamp(int(row[1]) / 1000, datetime.timezone.utc) if row[1] and int(row[1]) > 0 else ''
+        data_list.append((start, stop, row[2], row[3], row[4], row[5], row[6]))
+
+    data_headers = (('Start Time', 'datetime'), ('Stop Time', 'datetime'), 'Timezone', 'App ID', 'APP Sub ID', 'Duration', 'Duration in Secs')
+    return data_headers, data_list, source_path
