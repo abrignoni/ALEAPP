bc0b3c9fee7c24546e0551857c9e3c1dd15c5d47
Brigs <abrignoni@gmail.com>
2026-06-19 20:26:36 -0400

    Convert burnerMessages, MicrosoftAuthenticator, installedappsVending, PumaUsers to LAVA @artifact_processor

--- scripts/artifacts/MicrosoftAuthenticator.py
+++ scripts/artifacts/MicrosoftAuthenticator.py
@@ -1,7 +1,5 @@
-# pylint: disable=W0612,W0613,W1309
+# pylint: disable=W0613
 __artifacts_v2__ = {
-
-    
     "get_MSAuth_accounts": {
         "name": "Microsoft Authenticator - Accounts",
         "description": "Parses the existing Accounts out of the Microsoft Authenticator App.",
@@ -11,72 +9,36 @@ __artifacts_v2__ = {
         "requirements": "",
         "category": "MS Authenticator",
         "notes": "Get Account information from MS authenticator app.",
-        "paths": ('*/com.azure.authenticator/databases/PhoneFactor*'),
-        "output_types": None,
+        "paths": ('*/com.azure.authenticator/databases/PhoneFactor*',),
+        "output_types": ['html', 'tsv', 'lava'],
         "artifact_icon": "shield",
-        "function": "get_MSAuth_accounts",
     }
 }
 
-# Microsoft Authenticator (com.azure.authenticator)
-# Author:  Marco Neumann (kalinko@be-binary.de)
-# Version: 0.0.1
-# 
-# Tested with the following versions:
-# 2024-05-11: Android 14, App: 6.2404.2229
-
-# Requirements: -
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly
+from scripts.ilapfuncs import artifact_processor, logfunc, open_sqlite_db_readonly
 
 
-
-
+@artifact_processor
 def get_MSAuth_accounts(files_found, report_folder, seeker, wrap_text):
     logfunc("Processing data for Microsoft Authenticator - Accounts")
- 
-    accountUUIDS = []
-    mail_addresses = []
- 
-    files_found = [x for x in files_found if not x.endswith('wal') and not x.endswith('shm')]
-    
+
+    files_found = [x for x in files_found if not str(x).endswith('wal') and not str(x).endswith('shm')]
+
+    data_list = []
+    source_path = ''
     for file_found in files_found:
-        db = open_sqlite_db_readonly(file_found)
+        source_path = str(file_found)
+        db = open_sqlite_db_readonly(source_path)
         cursor = db.cursor()
         cursor.execute('''
             SELECT name, username, oath_secret_key
             FROM accounts
         ''')
         data_rows = cursor.fetchall()
-        
-        if len(data_rows):
-            logfunc(f"Found {len(data_rows)}  Microsoft Authenticator - Accounts")
-
-            description = f"Existing accounts in the Microsoft Authenticator App. These accounts are set up for 2FA on the device. Service Name is a user given label that can give hint to existing cloud accounts."
-            report = ArtifactHtmlReport('Microsoft Authenticator - Accounts')
-            report.start_artifact_report(report_folder, 'Microsoft Authenticator - Accounts', description)
-            report.add_script()
-            data_headers = ('Service Name', 'User Name', 'OATH Secret Key')
-            data_list = []
-            for row in data_rows:
-                service_name = row[0]
-                user_name = row[1]
-                secret = row[2]
-
-                data_list.append((service_name, user_name, secret))                          
-            tableID = 'msauth_accounts'
-
-            report.write_artifact_data_table(data_headers, data_list, ','.join(files_found))
-            report.end_artifact_report()
-
-            tsvname = f'MS Authenticator - Accounts'
-            tsv(report_folder, data_headers, data_list, tsvname)
-
-            tlactivity = f'MS Authenticator - Accounts'
-            timeline(report_folder, tlactivity, data_list, data_headers)
-        else:
-            logfunc('No entries found for MS Authenticator.')
-
         db.close()
-        
-        
+
+        for row in data_rows:
+            data_list.append((row[0], row[1], row[2]))
+
+    data_headers = ('Service Name', 'User Name', 'OATH Secret Key')
+    return data_headers, data_list, source_path
--- scripts/artifacts/PumaUsers.py
+++ scripts/artifacts/PumaUsers.py
@@ -1,4 +1,4 @@
-# pylint: disable=W0613,W1309
+# pylint: disable=W0613
 __artifacts_v2__ = {
     "get_puma_users": {
         "name": "PumaUsers",
@@ -10,70 +10,40 @@ __artifacts_v2__ = {
         "category": "Puma-Trac",
         "notes": "",
         "paths": ('*com.pumapumatrac/databases/pumatrac-db*',),
-        "output_types": None,
+        "output_types": "standard",
         "artifact_icon": "user",
-        "function": "get_puma_users",
+        "html_columns": ['Profile Image URL'],
     }
 }
 
-# Get Information related to the users table in the Puma Trac database
-# Author: Fabian Nunes {fabiannunes12@gmail.com}
-# Date: 2023-03-25
-# Version: 1.0
-# Requirements: Python 3.7 or higher
+import datetime
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, open_sqlite_db_readonly
+from scripts.ilapfuncs import artifact_processor, logfunc, open_sqlite_db_readonly
 
 
+@artifact_processor
 def get_puma_users(files_found, report_folder, seeker, wrap_text):
-    logfunc("Processing data for Puma Users")
-    files_found = [x for x in files_found if not x.endswith('wal') and not x.endswith('shm')]
-    file_found = str(files_found[0])
-    db = open_sqlite_db_readonly(file_found)
 
-    # Get information from the table device_sync_audit
+    files_found = [x for x in files_found if not str(x).endswith('wal') and not str(x).endswith('shm')]
+    source_path = str(files_found[0])
+    db = open_sqlite_db_readonly(source_path)
     cursor = db.cursor()
     cursor.execute('''
-        Select id, email, name, sex, datetime("dateOfBirth"/1000,'unixepoch'), weight, height, country, location, interestsIds, profileImageUrl, totalScore, followingCount, followersCount, goal_id, preferences_workoutTimeOfDay, preferences_workoutDuration
+        Select id, email, name, sex, dateOfBirth, weight, height, country, location, interestsIds,
+               profileImageUrl, totalScore, followingCount, followersCount, goal_id,
+               preferences_workoutTimeOfDay, preferences_workoutDuration
         from users
     ''')
-
     all_rows = cursor.fetchall()
-    usageentries = len(all_rows)
-    if usageentries > 0:
-        logfunc(f"Found {usageentries} entries in users")
-        report = ArtifactHtmlReport('User')
-        report.start_artifact_report(report_folder, 'Puma User')
-        report.add_script()
-        data_headers = ('ID', 'Email', 'Name', 'Gender', 'Date of Birth', 'Weight', 'Height', 'Country', 'Location', 'Interests', 'Profile Image URL', 'Total Score', 'Following Count', 'Followers Count', 'Goal', 'Workout Time of Day', 'Workout Duration')
-        data_list = []
-        for row in all_rows:
-            work_time = row[16]
-            if work_time:
-                # convert to minutes
-                work_time = work_time / 60
-            else:
-                work_time = 'N/A'
-            if row[10]:
-                image = '<img src="'+row[10]+'" alt="'+row[10]+'" width="50" height="50">'
-            else:
-                image = 'N/A'
-
-            data_list.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], image, row[11], row[12], row[13], row[14], row[15], work_time))
-
-        # Filter by date
-        table_id = "PumaUsers"
-        report.write_artifact_data_table(data_headers, data_list, file_found, table_id=table_id, html_escape=False)
-        report.end_artifact_report()
-
-        tsvname = f'Puma - User'
-        tsv(report_folder, data_headers, data_list, tsvname)
-
-        tlactivity = f'Puma - User'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-
-    else:
-        logfunc('No Puma Users data available')
-
     db.close()
+    logfunc(f"Found {len(all_rows)} entries in users")
+
+    data_list = []
+    for row in all_rows:
+        dob = datetime.datetime.fromtimestamp(int(row[4]) / 1000, datetime.timezone.utc) if row[4] else ''
+        work_time = row[16] / 60 if row[16] else 'N/A'
+        image = '<img src="' + row[10] + '" alt="' + row[10] + '" width="50" height="50">' if row[10] else 'N/A'
+        data_list.append((row[0], row[1], row[2], row[3], dob, row[5], row[6], row[7], row[8], row[9], image, row[11], row[12], row[13], row[14], row[15], work_time))
+
+    data_headers = ('ID', 'Email', 'Name', 'Gender', ('Date of Birth', 'datetime'), 'Weight', 'Height', 'Country', 'Location', 'Interests', 'Profile Image URL', 'Total Score', 'Following Count', 'Followers Count', 'Goal', 'Workout Time of Day', 'Workout Duration')
+    return data_headers, data_list, source_path
