f369a91dbb013b9a9813523349e9fb379033aeef
Brigs <abrignoni@gmail.com>
2026-06-20 15:18:51 -0400

    Convert smyfilesRecents and wellbeing to LAVA @artifact_processor (multi-report splits)

--- scripts/artifacts/smyfilesRecents.py
+++ scripts/artifacts/smyfilesRecents.py
@@ -1,7 +1,7 @@
-# pylint: disable=W0611,W0613,W0631,W0702,W1309
+# pylint: disable=W0613,W0718
 __artifacts_v2__ = {
     "get_smyfilesRecents": {
-        "name": "smyfilesRecents",
+        "name": "My Files - Recent Files",
         "description": "",
         "author": "",
         "creation_date": "2020-03-21",
@@ -10,114 +10,96 @@ __artifacts_v2__ = {
         "category": "My Files",
         "notes": "",
         "paths": ('*/com.sec.android.app.myfiles/databases/myfiles.db*', '*/com.sec.android.app.myfiles/databases/FileInfo.db*'),
-        "output_types": None,
+        "output_types": "standard",
         "artifact_icon": "file",
-        "function": "get_smyfilesRecents",
+    },
+    "get_smyfilesRecents_fileinfo": {
+        "name": "My Files - Recent Files (FileInfo)",
+        "description": "",
+        "author": "",
+        "creation_date": "2020-03-21",
+        "last_update_date": "2020-03-21",
+        "requirements": "none",
+        "category": "My Files",
+        "notes": "",
+        "paths": ('*/com.sec.android.app.myfiles/databases/myfiles.db*', '*/com.sec.android.app.myfiles/databases/FileInfo.db*'),
+        "output_types": "standard",
+        "artifact_icon": "file",
     }
 }
 
-import sqlite3
-import textwrap
+import datetime
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly
+from scripts.ilapfuncs import artifact_processor, logfunc, open_sqlite_db_readonly
 
-def get_smyfilesRecents(files_found, report_folder, seeker, wrap_text):
-    
+
+def _ms_to_utc(value):
+    if value:
+        return datetime.datetime.fromtimestamp(int(value) / 1000, datetime.timezone.utc)
+    return ''
+
+
+def _myfiles_db(files_found):
     for file_found in files_found:
         file_found = str(file_found)
-        
         if file_found.endswith('.db'):
-            break
-        
-    db = open_sqlite_db_readonly(file_found)
-    cursor = db.cursor()
-    try:
-        cursor.execute('''
-        select
-        datetime(date_modified / 1000, "unixepoch"),
-        name,
-        size,
-        _data,
-        ext,
-        _source,
-        _description,
-        datetime(recent_date / 1000, "unixepoch")
-        from recent_files 
-        ''')
+            return file_found
+    return ''
 
-        all_rows = cursor.fetchall()
-        usageentries = len(all_rows)
-    except:
-        usageentries = 0    
-        
-    if usageentries > 0:
-        report = ArtifactHtmlReport('My Files DB - Recent Files')
-        report.start_artifact_report(report_folder, 'My Files DB - Recent Files')
-        report.add_script()
-        data_headers = ('Timestamp','Name','Size','Data','Ext.', 'Source', 'Description', 'Recent Timestamp' ) 
-        data_list = []
-        for row in all_rows:
-            data_list.append((row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]))
 
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
-        
-        tsvname = f'my files db - recent files'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = f'My Files DB - Recent Files'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-    else:
-        logfunc('No My Files DB Recents data available')
-    
-    try:
-        cursor.execute('''
-        SELECT
-        datetime (recent_files.recent_date/1000, "unixepoch") as "Recent Date",
-        datetime(recent_files.date_modified/1000, "unixepoch") as "Recent Files",
-        recent_files.file_id as "File ID",
-        recent_files.package_name as "Package Name",
-        recent_files.path as "Path",
-        recent_files.size as "Size",
-        case recent_files.is_download
-            WHEN '1' THEN "True"
-            WHEN '0' THEN "False"
-        end "Downloaded?",
-        case recent_files.is_hidden
-            WHEN '1' THEN "True"
-            WHEN '0' THEN "False"
-        end "Hidden",
-        case recent_files.is_trashed
-            WHEN '1' then  "True"
-            when '0' then "False"
-        end "Trashed"
-        from recent_files
-        ''')
-        
-        all_rows = cursor.fetchall()
-        usageentries = len(all_rows)
-    except:
-        usageentries = 0    
-        
-    if usageentries > 0:
-        report = ArtifactHtmlReport('My Files DB - Recent Files')
-        report.start_artifact_report(report_folder, 'My Files DB - Recent Files')
-        report.add_script()
-        data_headers = ('Recent Timestamp','Modified Timestamp','File ID','Package Name','Path', 'Size', 'Is Downloaded?', 'Is Hidden?','Is Trashed?' ) 
-        data_list = []
+@artifact_processor
+def get_smyfilesRecents(files_found, report_folder, seeker, wrap_text):
+    data_list = []
+    source_path = _myfiles_db(files_found)
+    if source_path:
+        db = open_sqlite_db_readonly(source_path)
+        cursor = db.cursor()
+        try:
+            cursor.execute('''
+                select date_modified, name, size, _data, ext, _source, _description, recent_date
+                from recent_files
+            ''')
+            all_rows = cursor.fetchall()
+        except Exception as e:
+            logfunc(str(e))
+            all_rows = []
+        db.close()
         for row in all_rows:
-            data_list.append((row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]))
-            
-        report.write_artifact_data_table(data_headers, data_list, file_found)
-        report.end_artifact_report()
-        
-        tsvname = f'my files db - recent files'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-        tlactivity = f'My Files DB - Recent Files'
-        timeline(report_folder, tlactivity, data_list, data_headers)
-    else:
-        logfunc('No My Files DB Recents data available')
-    
-    db.close()
+            data_list.append((_ms_to_utc(row[0]), row[1], row[2], row[3], row[4], row[5], row[6], _ms_to_utc(row[7])))
+
+    data_headers = (('Timestamp', 'datetime'), 'Name', 'Size', 'Data', 'Ext.', 'Source', 'Description', ('Recent Timestamp', 'datetime'))
+    return data_headers, data_list, source_path
+
+
+@artifact_processor
+def get_smyfilesRecents_fileinfo(files_found, report_folder, seeker, wrap_text):
+    data_list = []
+    source_path = _myfiles_db(files_found)
+    if source_path:
+        db = open_sqlite_db_readonly(source_path)
+        cursor = db.cursor()
+        try:
+            cursor.execute('''
+                SELECT
+                recent_files.recent_date,
+                recent_files.date_modified,
+                recent_files.file_id,
+                recent_files.package_name,
+                recent_files.path,
+                recent_files.size,
+                case recent_files.is_download WHEN '1' THEN "True" WHEN '0' THEN "False" end,
+                case recent_files.is_hidden WHEN '1' THEN "True" WHEN '0' THEN "False" end,
+                case recent_files.is_trashed WHEN '1' then "True" when '0' then "False" end
+                from recent_files
+            ''')
+            all_rows = cursor.fetchall()
+        except Exception as e:
+            logfunc(str(e))
+            all_rows = []
+        db.close()
+        for row in all_rows:
+            data_list.append((_ms_to_utc(row[0]), _ms_to_utc(row[1]), row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
+
+    data_headers = (('Recent Timestamp', 'datetime'), ('Modified Timestamp', 'datetime'), 'File ID', 'Package Name',
+                    'Path', 'Size', 'Is Downloaded?', 'Is Hidden?', 'Is Trashed?')
+    return data_headers, data_list, source_path
