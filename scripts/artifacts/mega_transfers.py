0f86849d80244efffe661ccf1f2cd1e70d5b8efa
Brigs <abrignoni@gmail.com>
2026-06-19 21:42:09 -0400

    Convert vaulty_info, mega_transfers, AdidasUser to LAVA @artifact_processor

--- scripts/artifacts/mega_transfers.py
+++ scripts/artifacts/mega_transfers.py
@@ -1,4 +1,4 @@
-# pylint: disable=W0108,W0613,W1309
+# pylint: disable=W0613
 __artifacts_v2__ = {
     "get_mega_transfers": {
         "name": "mega_transfers",
@@ -10,103 +10,56 @@ __artifacts_v2__ = {
         "category": "Mega",
         "notes": "",
         "paths": ('*/mega.privacy.android.app/databases/megapreferences',),
-        "output_types": None,
+        "output_types": "standard",
         "artifact_icon": "download",
-        "function": "get_mega_transfers",
     }
 }
 
-import sqlite3
 import base64
-from Crypto.Cipher import AES
 import datetime
 
-from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv
+from Crypto.Cipher import AES
+
+from scripts.ilapfuncs import artifact_processor, open_sqlite_db_readonly
+
+DIRECTION = {"0": "Download", "1": "Upload", "2": "Download"}
+STATE = {
+    "0": "None", "1": "Queued", "2": "Active", "3": "Paused", "4": "Retrying",
+    "5": "Completing", "6": "Completed", "7": "Cancelled", "8": "Failed",
+}
 
 
-
-def decrypt(cell: str):
+def decrypt(cell):
     cipher_text = base64.b64decode(cell)
     secret_key = "android_idfkvn8 w4y*(NC$G*(G($*GR*(#)*huio4h389$G"[0:32].encode('utf-8')
     algorithm = AES.new(secret_key, AES.MODE_ECB)
     plain_text = algorithm.decrypt(cipher_text).decode('utf-8')
     last_char = plain_text[-1]
     first_of_the_last_chars = plain_text.index(last_char)
-    plain_text = plain_text[:first_of_the_last_chars]
-    return plain_text
+    return plain_text[:first_of_the_last_chars]
 
 
+@artifact_processor
 def get_mega_transfers(files_found, report_folder, seeker, wrap_text):
-    
-    db_filename = str(files_found[0])
 
-    conn = sqlite3.connect(db_filename)
-    cursor = conn.cursor()
-
-    sql = """
-        SELECT
-            transfertimestamp,
-            transferpath,
-            transferfilename,
-            transfersize,
-            transfertype,
-            transferstate,
-            transferoriginalpath
-        FROM 
-            completedtransfers
-        """
-    cursor.execute(sql)
+    source_path = str(files_found[0])
+    db = open_sqlite_db_readonly(source_path)
+    cursor = db.cursor()
+    cursor.execute('''
+        SELECT transfertimestamp, transferpath, transferfilename, transfersize,
+               transfertype, transferstate, transferoriginalpath
+        FROM completedtransfers
+    ''')
     results = cursor.fetchall()
-    conn.close()
+    db.close()
 
-    data_headers = [
-            "Timestamp",
-            "Mega Folder",
-            "Filename",
-            "Size",
-            "Direction",
-            "State",
-            "Transfer Path",
-            ]
     data_list = []
-
-    direction = {
-            "0": "Download",
-            "1": "Upload",
-            "2": "Download",
-            }
-
-    state = {
-            "0": "None",
-            "1": "Queued",
-            "2": "Active",
-            "3": "Paused",
-            "4": "Retrying",
-            "5": "Completing",
-            "6": "Completed",
-            "7": "Cancelled",
-            "8": "Failed",
-            }
-
     for r in results:
-        decrypted = list(map(lambda x: decrypt(x), r))
-        timestamp = datetime.datetime.utcfromtimestamp(int(decrypted[0]) / 1000)
-        decrypted[0] = f"{timestamp}" 
-        decrypted[4] = direction[decrypted[4]]
-        decrypted[5] = state[decrypted[5]]
+        decrypted = [decrypt(x) for x in r]
+        decrypted[0] = datetime.datetime.fromtimestamp(int(decrypted[0]) / 1000, datetime.timezone.utc)
+        decrypted[4] = DIRECTION.get(decrypted[4], decrypted[4])
+        decrypted[5] = STATE.get(decrypted[5], decrypted[5])
         data_list.append(decrypted)
 
-    if data_list:
-        report = ArtifactHtmlReport('MEGA - Files')
-        report.start_artifact_report(report_folder, "MEGA - Files")
-        report.add_script()
-        report.write_artifact_data_table(data_headers, data_list, db_filename)
-        report.end_artifact_report()
-        
-        tsvname = f'MEGA - Files'
-        tsv(report_folder, data_headers, data_list, tsvname)
-        
-    else:
-        logfunc('No MEGA files data available')
-    
+    data_headers = (('Timestamp', 'datetime'), 'Mega Folder', 'Filename', 'Size', 'Direction', 'State', 'Transfer Path')
+    return data_headers, data_list, source_path
